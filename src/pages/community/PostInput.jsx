16edfe187e694f89753b1b62741f4115563b6aaa
kyulipark <kyuli94p@gmail.com>
2024-09-13 00:12:58 +0900

    add: community 초기 세팅

--- src/pages/community/PostInput.jsx
+++ src/pages/community/PostInput.jsx
@@ -1,15 +1,15 @@
 const PostInput = () => {
   return (
-    <div className="w-[300px] h-[400px] bg-gray-100 border-2 border-primary p-4 m-10 flex flex-col justify-center items-center">
-      <h3>닉네임</h3>
+    <div className="max-w-80 max-h-100 bg-gray-100 border-2 rounded border-primary p-4 m-10 flex flex-col justify-center items-center">
+      <h3 className="self-start">닉네임</h3>
 
       <textarea
-        className="w-full h-40 border-solid border-gray-300 p-2 m-4"
+        className="w-full h-40 border-solid border-gray-300 p-2 m-4 cursor-text"
         placeholder="내용을 입력하세요"
       />
       <div className="flex justify-between items-center w-full">
-        <button className="text-gray-400 "> 파일 업로드</button>
-        <button className="bg-white text-black border-solid border-primary hover:bg-primary hover:text-white p-1 transition duration-300">
+        <button className="text-gray-400 cursor-pointer "> 파일 업로드</button>
+        <button className="bg-white text-black border-solid rounded border-primary hover:bg-primary hover:text-white p-1 transition duration-300 cursor-pointer">
           완료
         </button>
       </div>
