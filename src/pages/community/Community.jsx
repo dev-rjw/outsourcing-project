f5b2cafa4fa2ff6ed2dc8144fc352e5cae259c45
kyulipark <kyuli94p@gmail.com>
2024-09-12 23:46:55 +0900

    add: community 초기 세팅

--- src/pages/community/Community.jsx
+++ src/pages/community/Community.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import FilterBar from "./FilterBar";
+import PostInput from "./postInput";
+import PostList from "./postList";
 
 const Community = () => {
   return (
-    <div>Community</div>
-  )
-}
+    <div className="bg-[#d3d3d3] h-screen">
+      <FilterBar />
+      <PostInput />
+      <PostList />
+    </div>
+  );
+};
 
-export default Community
+export default Community;
---
+++ src/pages/community/PostList.jsx
@@ -0,0 +1,5 @@
+const PostList = () => {
+  return <div className="m-10">PostList</div>;
+};
+
+export default PostList;
