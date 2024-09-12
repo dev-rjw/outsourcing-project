9fb053945c82d2ee3c006ce62e64bc4e8565bb19
ekyeon715 <rkdwldus9636@naver.com>
2024-09-12 17:27:07 +0900

    [update]  기본 틀 작성 완료

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div>
-      <header>
-        <nav>
-          <Link>홈버튼(프로젝트명으로 바꾸기)</Link>
-          <Link>장르별</Link>
-          <Link>장소별</Link>
-          <input></input>
+      <header className="flex-col m-4">
+        <Link to="/">커튼콜</Link>
+        <nav className="flex ">
+          <div className="flex ">
+            <Link to="/category">장르별</Link>
+            <Link to="/category">장소별</Link>
+            <Link to="/community">커뮤니티</Link>
+          </div>
+          <div className="flex ">
+            <input placeholder="검색내용입력"></input>
+            <button>검색이모지</button>
+          </div>
         </nav>
       </header>
+      <main>{children}</main>
+      <footer className="flex justify-between  ">
+        <h1>5진스 강다연 류지원 박규리 정소현 조해인</h1>
+        <h1>ⓒ 2024. 5진스 All rights reserved.</h1>
+      </footer>
     </div>
   );
 };
