e59c8b2dde8ad8cbc1a510bad9b6fa9629ea5b0e
joycie416 <joycie416@gmail.com>
2024-09-12 15:27:00 +0900

    [add] router setting

---
+++ src/shared/Route.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from '../pages/main-page/MainPage'
+import Category from '../pages/category/Category'
+import DetailPage from '../pages/detail-page/DetailPage'
+import Community from '../pages/community/Community'
+
+const RouterComponent = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/category' element={<Category />} />
+        <Route path='/detail' element={<DetailPage />} />
+        <Route path='/community' element={<Community />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouterComponent
