629cd11ce7b55a5550ffd40c1781b9c7315736e9
kyulipark <kyuli94p@gmail.com>
2024-09-13 03:23:12 +0900

    update: img넣을 수 있는 ui로 변경

--- src/pages/community/PostInput.jsx
+++ src/pages/community/PostInput.jsx
@@ -16,7 +16,8 @@ const PostInput = ({ addPost }) => {
   };
 
   return (
-    <div className="max-w-80 max-h-100 bg-gray-100 border-2 rounded border-primary p-4 m-10 flex flex-col justify-center items-center">
+    <div className="max-w-100 max-h-100 bg-gray-100 border-2 rounded border-primary p-4 m-10 flex flex-col justify-center items-center">
+      <div className="bg-gray-300 h-40 w-full text-gray-400">파일 업로드</div>
       <div className="w-full flex justify-between">
         <h3>닉네임</h3>
       </div>
@@ -27,7 +28,7 @@ const PostInput = ({ addPost }) => {
         onChange={(e) => setContent(e.target.value)}
       />
       <div className="flex justify-between items-center w-full">
-        <button className="text-gray-300 cursor-pointer "> 파일 업로드</button>
+        <button className="text-gray-300 cursor-pointer "> #공연 제목</button>
         <button
           onClick={handleSubmit}
           className="text-black border-solid rounded border-primary hover:bg-primary hover:text-white  transition duration-300 cursor-pointer w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex justify-center items-center "
