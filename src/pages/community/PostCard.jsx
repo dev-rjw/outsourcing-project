83c23bea775b17963fd24397ca6862498feb914c
kyulipark <kyuli94p@gmail.com>
2024-09-13 03:23:12 +0900

    update: img넣을 수 있는 ui로 변경

--- src/pages/community/PostCard.jsx
+++ src/pages/community/PostCard.jsx
@@ -21,9 +21,8 @@ const PostCard = ({ post, onDelete, onUpdate }) => {
   };
 
   return (
-    <div className="max-w-80 max-h-100 bg-gray-100 border-2 rounded border-primary p-4 m-10 ">
-      <div className="w-full flex justify-between">
-        <h3>닉네임</h3>
+    <div className="max-w-100 max-h-100 bg-gray-100 border-2 rounded border-primary p-4 m-10 ">
+      <div className="w-full flex justify-end">
         <PostLike />
       </div>
 
@@ -47,22 +46,34 @@ const PostCard = ({ post, onDelete, onUpdate }) => {
       ) : (
         // 수정 모드가 아닐 때
         <div>
-          <div className="w-full h-40  p-2 m-4">
-            <p>{post.content}</p>
+          <div className="w-full h-full  p-2 m-4">
+            <div className="bg-gray-300 h-40">img</div>
+            <div className="flex justify-start gap-2">
+              <p className="text-black"> 닉네임</p>
+              <p className="text-gray-300 text-xs font-light flex justify-start items-center ">
+                {" "}
+                2024.09.13
+              </p>
+            </div>
+            <p className="bg-white h-20">{post.content}</p>
           </div>
-          <div className="flex gap-4 flex-start">
-            <button
-              onClick={toggleEdit}
-              className="text-gray-300 hover:text-primary transition duration-300 cursor-pointer"
-            >
-              수정하기
-            </button>
-            <button
-              onClick={() => onDelete(post.id)}
-              className="text-gray-300 hover:text-primary transition duration-300 cursor-pointer"
-            >
-              삭제하기
-            </button>
+
+          <div className="flex justify-between items-center">
+            <p className="text-left text-primary">#공연제목</p>
+            <div className="flex gap-4 justify-end">
+              <button
+                onClick={toggleEdit}
+                className="text-gray-300 hover:text-primary transition duration-300 cursor-pointer"
+              >
+                수정하기
+              </button>
+              <button
+                onClick={() => onDelete(post.id)}
+                className="text-gray-300 hover:text-primary transition duration-300 cursor-pointer"
+              >
+                삭제하기
+              </button>
+            </div>
           </div>
         </div>
       )}
