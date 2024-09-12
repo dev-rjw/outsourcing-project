6ea6c4a911c7405ad972ba82044629038df83172
ekyeon715 <rkdwldus9636@naver.com>
2024-09-12 17:28:55 +0900

    [fix] 코드 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,14 +11,14 @@ const Layout = ({ children }) => {
             <Link to="/category">장소별</Link>
             <Link to="/community">커뮤니티</Link>
           </div>
-          <div className="flex ">
+          <div className="flex">
             <input placeholder="검색내용입력"></input>
             <button>검색이모지</button>
           </div>
         </nav>
       </header>
       <main>{children}</main>
-      <footer className="flex justify-between  ">
+      <footer className="flex justify-between">
         <h1>5진스 강다연 류지원 박규리 정소현 조해인</h1>
         <h1>ⓒ 2024. 5진스 All rights reserved.</h1>
       </footer>
