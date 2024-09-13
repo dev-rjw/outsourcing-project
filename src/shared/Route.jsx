cb93406182c8633fbcb9e71a0d7adf8cd29e0fec
kyulipark <kyuli94p@gmail.com>
2024-09-13 15:21:57 +0900

    update: dev pull

--- src/shared/Route.jsx
+++ src/shared/Route.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainPage from '../pages/main-page/MainPage'
-import Category from '../pages/category/Category'
-import DetailPage from '../pages/detail-page/DetailPage'
-import Community from '../pages/community/Community'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "../pages/main-page/MainPage";
+import Category from "../pages/category/Category";
+import DetailPage from "../pages/detail-page/DetailPage";
+import Community from "../pages/community/Community";
 
 const RouterComponent = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/category' element={<Category />} />
-        <Route path='/detail' element={<DetailPage />} />
-        <Route path='/community' element={<Community />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/community" element={<Community />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default RouterComponent
+export default RouterComponent;
