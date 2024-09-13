64069282fe7eaaa6b4d731cf02732c7cc503e7f9
dev-rjw <rjw0105@gmail.com>
2024-09-13 16:23:41 +0900

    [feat] Header & Footer 추가 및 README 내용 추가

--- src/shared/Route.jsx
+++ src/shared/Route.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainPage from '../pages/main-page/MainPage'
-import Category from '../pages/category/Category'
-import DetailPage from '../pages/detail-page/DetailPage'
-import Community from '../pages/community/Community'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "../pages/main-page/MainPage";
+import Category from "../pages/category/Category";
+import DetailPage from "../pages/detail-page/DetailPage";
+import Community from "../pages/community/Community";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const RouterComponent = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/category' element={<Category />} />
-        <Route path='/detail' element={<DetailPage />} />
-        <Route path='/community' element={<Community />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/community" element={<Community />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default RouterComponent
+export default RouterComponent;
