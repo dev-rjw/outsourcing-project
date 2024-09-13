9b935f59871d3444d0e8e4526c30a1ae7d68fb8f
kyulipark <kyuli94p@gmail.com>
2024-09-13 17:52:54 +0900

    [update] dev pull

--- src/shared/Route.jsx
+++ src/shared/Route.jsx
@@ -5,6 +5,7 @@ import DetailPage from "../pages/detail-page/DetailPage";
 import Community from "../pages/community/Community";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Login from "../pages/login/Login";
 
 const RouterComponent = () => {
   return (
@@ -15,6 +16,7 @@ const RouterComponent = () => {
         <Route path="/category" element={<Category />} />
         <Route path="/detail" element={<DetailPage />} />
         <Route path="/community" element={<Community />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </BrowserRouter>
