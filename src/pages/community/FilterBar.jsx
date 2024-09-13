c5cc6141788809d1c7febe3c0d38205b093e5451
kyulipark <kyuli94p@gmail.com>
2024-09-13 17:21:00 +0900

    update: tailwind 공통 버튼 css index.css에 작업

--- src/pages/community/FilterBar.jsx
+++ src/pages/community/FilterBar.jsx
@@ -1,13 +1,9 @@
 const FilterBar = () => {
   return (
     <div className="flex justify-start items-center space-x-4 p-4 ml-6">
-      <button className="text-main hover:text-primary focus:outline-none">
-        최신순
-      </button>
+      <button className="softBtn text-main">최신순</button>
       <span className="text-main">|</span>
-      <button className="text-main hover:text-primary focus:outline-none">
-        인기순
-      </button>
+      <button className="softBtn text-main">인기순</button>
     </div>
   );
 };
