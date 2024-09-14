b6cc99cf9965ef4d82944662583a8ae4b140111b
joycie416 <joycie416@gmail.com>
2024-09-14 18:10:31 +0900

    [update] 캐러셀 공연 내용 두 개 보이도록 수정. 하단 카드 정렬 수정 및  클릭이벤트 추가

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import RecentPlays from './RecentPlays'
 import Genre from './Genre'
+import Embla from './Embla'
 
 const MainPage = () => {
 
   return (
     <div className='main-body max-w-screen-lg w-full mx-auto flex flex-col items-center'>
-      <h3>MainPage</h3>
-      <RecentPlays />
+      <Embla />
       <Genre/>
     </div>
   )
