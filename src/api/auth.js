f4d7839da19825eaefecbfcaffdc4b7052a4f514
ekyeon715 <rkdwldus9636@naver.com>
2024-09-14 15:42:20 +0900

    [update] 프로필 입력 및 업데이트 함수 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -11,3 +11,21 @@ export const login = async (userData) => {
   const response = await axios.post(`${API_URL}/login`, userData);
   return response.data;
 };
+
+export const getUserProfile = async (token) => {
+  const response = await axios.post(`${API_URL}/user`, {
+    header: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const updateUserProfile = async (formData, token) => {
+  const response = await axios.post(`${API_URL}/user`, formData, {
+    header: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
