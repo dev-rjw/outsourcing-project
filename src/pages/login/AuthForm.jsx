529a948b1059a08d22de7eb6e0af1d210e060604
ekyeon715 <rkdwldus9636@naver.com>
2024-09-14 15:43:44 +0900

    [update] 회원가입 및 로그인 폼 구현

--- src/pages/login/AuthForm.jsx
+++ src/pages/login/AuthForm.jsx
@@ -1,5 +1,66 @@
-const AuthForm = () => {
-  return <div>AuthForm</div>;
+import { useState } from "react";
+
+const AuthForm = ({ mode, onSubmit }) => {
+  const [formData, setFornData] = useState({
+    id: "",
+    password: "",
+    nickname: mode === "signup" ? "" : undefined,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFornData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const hangleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
+  return (
+    <form onSubmit={hangleSubmit}>
+      <input
+        type="text"
+        name="id"
+        value={formData.id}
+        onChange={handleChange}
+        placeholder="아이디"
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        placeholder="비밀번호"
+        required
+      />
+      {/* {mode === "signup" && (
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="비밀번호확인"
+          required
+        />
+      )} */}
+      {mode === "signup" && (
+        <input
+          type="text"
+          name="nickname"
+          value={formData.nickname}
+          onChange={handleChange}
+          placeholder="닉네임"
+          required
+        />
+      )}
+      <button type="submit">{mode === "login" ? "로그인" : "회원가입"}</button>
+    </form>
+  );
 };
 
 export default AuthForm;
