6617cd7c565bb6f3a27cfbe6a307f1a2d7eb4d63
joycie416 <joycie416@gmail.com>
2024-09-14 18:10:31 +0900

    [update] 캐러셀 공연 내용 두 개 보이도록 수정. 하단 카드 정렬 수정 및  클릭이벤트 추가

--- src/pages/main-page/Genre.jsx
+++ src/pages/main-page/Genre.jsx
@@ -7,30 +7,56 @@ const Genre = () => {
   const genreArray = Object.values(genreCodes);
 
   const { data: genre, isPending, isError } = useQuery({
-    queryKey: ['genre'],
+    queryKey: ['main-genre'],
     queryFn: getClassifiedData,
   })
 
   // console.log(genre);
 
+  if (isPending) {
+    return (
+      <div className='w-full h-[400px] flex items-center'>
+        <p className='m-auto'>로딩중입니다.</p>
+      </div>
+    )
+  }
+  if (isError) {
+    return (
+      <div className='w-full h-[400px] flex items-center'>
+        <p className='m-auto'>데이터 조회 중 오류가 발생했습니다.</p>
+      </div>
+    )
+  }
+
   return (
     <div className='w-full flex flex-col items-center'>
-      <h3>Genre</h3>
+      {/* <h3>Genre</h3> */}
       {genre?.map((plays, idx) => {
         if (plays.length === 0) {
           return null;
         } else {
+          const tmp = [...plays];
+          while (tmp.length < 5) {
+            tmp.push(null);
+          }
+          // console.log(genreArray[idx], tmp)
+
           return (
-            <div className='mb-10' key={genreArray[idx]}>
-              <p>{genreArray[idx]}</p>
+            <div className='w-full mb-10' key={genreArray[idx]}>
+              <p className='text-primary'>{genreArray[idx]}</p>
               <div className='w-full grid grid-cols-[repeat(5,1fr)] gap-x-10'>
-                {plays.map((play, idx) => (
-                  <div className='w-full min-w-[150px] h-fit max-h-[330px]' key={play.prfnm}>
-                    <img className='w-full h-full max-h-[250px] aspect-[3/4] object-cover' src={play.poster} key={play.prfnm}/>
-                    <p className='genre_title'>{play.prfnm}</p>
-                    <p className='genre_place'>{play.fcltynm}</p>
-                  </div>
-                ))}
+                {tmp.map((play, i) => {
+                  console.log(genreArray[idx], i)
+                  console.log(Boolean(play))
+                  if (play) {
+                    return <Card play={play} />
+                  } else {
+                    return (
+                      <div className='w-[150px] h-fit max-h-[330px] bg-gray-500' key={i} />
+                    )
+                  }
+
+                })}
               </div>
             </div>
           )
@@ -43,3 +69,18 @@ const Genre = () => {
 
 export default Genre
 
+// =============================
+const Card = ({ play }) => {
+
+  const handleClick = () => {
+    alert(play.prfnm);
+  }
+  return (
+    <div className='w-full min-w-[150px] h-fit max-h-[330px] cursor-pointer' key={play.prfnm} 
+    onClick={handleClick}>
+      <img className='w-full h-full max-h-[250px] aspect-[3/4] object-cover' src={play.poster} key={play.prfnm} />
+      <p className='genre_title mb-1'>{play.prfnm}</p>
+      <p className='genre_place text-[#b1b1b1]'>{play.fcltynm}</p>
+    </div>
+  )
+}
