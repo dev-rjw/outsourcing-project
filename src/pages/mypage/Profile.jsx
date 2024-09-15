2a8dd139e879dd33cfaafd15d1fe8e0a80eb79ff
ekyeon715 <rkdwldus9636@naver.com>
2024-09-15 15:45:04 +0900

    [update] 프로필 수정 로직 추가 및 입력 폼 추가

--- src/pages/mypage/Profile.jsx
+++ src/pages/mypage/Profile.jsx
@@ -12,12 +12,29 @@ const Profile = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // try{
-    //     const response = await updateProfile({ nickname });
-    //     setUser({ ...user, nickname: response.nickname });
-    // } catch
+    try {
+      const formData = { nickname };
+      const updataUser = await updateProfile(formData, user.accessToken);
+      setUser(updataUser);
+      alert("프로필이 성공적으로 업데이트 되었습니다.");
+    } catch (error) {
+      alert("프로필 업데이트에 실패했습니다. 다시 시도해주세요.");
+    }
   };
-  return <div>Profile</div>;
+  return (
+    <div>
+      <div>
+        <h1>My Page</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>닉네임</label>
+            <input onChange={handleNicknameChange} />
+          </div>
+          <button type="submit">프로필 업데이트</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
