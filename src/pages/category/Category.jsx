a3e9f8447f7030e3aff65a1c15753d6c1a37c961
dev-rjw <rjw0105@gmail.com>
2024-09-18 18:28:28 +0900

    [feat] 카테고리 화면 기능 추가, 카테고리 관련 API 추가

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import { searchGenreAreaData } from "../../api/playApi";
+import { areaCodes, genreCodes } from "../../utils/Kopis-api-common";
+import { useState } from "react";
 
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+    const [searchValue, setSearchValue] = useState("");
+    const [genre, setGenre] = useState("장르별");
+    const [area, setArea] = useState("지역별");
+    const [end, setEnd] = useState(50);
 
-export default Category
+    /* TODO:
+    - 카테고리 변경 시 바로 값 바뀌도록 수정
+    - css 수정
+    */
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["category"],
+        queryFn: () => searchGenreAreaData(searchValue, genre, area, 0, end),
+    });
+
+    const addList = () => {
+        setEnd(end + 50);
+        searchGenreAreaData(searchValue, genre, area, 0, end);
+    };
+
+    if (isLoading) {
+        return <div>로딩중입니다...</div>;
+    }
+
+    if (isError) {
+        return <div>에러입니다...</div>;
+    }
+
+    return (
+        <div>
+            <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+            <button onClick={() => searchGenreAreaData(searchValue, genre, area, 0, end)}>검색</button>
+            <div>
+                <select value={area} onChange={(e) => setArea(e.target.value)} selected="지역별" name="지역별">
+                    <option value="지역별">지역별</option>
+                    {Object.values(areaCodes).map((element, index) => {
+                        return (
+                            <option key={index} value={element}>
+                                {element}
+                            </option>
+                        );
+                    })}
+                </select>
+                <select value={genre} onChange={(e) => setGenre(e.target.value)} selected="장르별" name="장르별">
+                    <option value="장르별">장르별</option>
+                    {Object.values(genreCodes).map((element, index) => {
+                        return (
+                            <option key={index} value={element}>
+                                {element}
+                            </option>
+                        );
+                    })}
+                </select>
+                {data?.map((element) => {
+                    return (
+                        <div key={element.id}>
+                            <img src={element.poster} width="10%" />
+                            <div>{element.prfnm}</div>
+                        </div>
+                    );
+                })}
+                {data.length === 50 && <button onClick={addList}>더보기</button>}
+            </div>
+        </div>
+    );
+};
+
+export default Category;
