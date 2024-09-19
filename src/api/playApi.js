3386a3df1d0cef6eb991a484019f9a438526ed77
dev-rjw <rjw0105@gmail.com>
2024-09-19 19:20:41 +0900

    [feat] rows 50개씩 가져오도록 수정

--- src/api/playApi.js
+++ src/api/playApi.js
@@ -4,35 +4,26 @@ import { getDateString, parseXMLToJSON } from "../utils/utils";
 
 // KOPIS 관련
 const BASE_URL = "http://kopis.or.kr/openApi/restful/pblprfr";
-// // const API_KEY = import.meta.env.VITE_KOPIS_KEY;
-// const url =
-//   BASE_URL +
-//   `?service=${API_KEY}&stdate=20240911&eddate=20240911&rows=10&cpage=1`;
-//   // 7e6379e8f4ad4bc5a8d668d4dfea6e78
+// const BASE_DB_URL = import.meta.env.VITE_DB_URL + "/performances";
 
-// const BASE_DB_URL = "http://localhost:5000/performances";
-const BASE_DB_URL = import.meta.env.VITE_DB_URL + "/performances";
-
-// const playApi = axios.create({ baseURL: BASE_DB_URL });
 const playApi = axios.create({ baseURL: BASE_URL });
 
 export const getData = async () => {
-  try {// const { data } = await playApi.get("/");
-  const { data } = await playApi.get("/",{
-    params : {
-      service: import.meta.env.VITE_KOPIS_KEY,
-      stdate: getDateString(),
-      eddate: getDateString(),
-      rows: 1000,
-      cpage: 1,
-    }
-  });
-  // console.log(parseXMLToJSON(data))
-  return parseXMLToJSON(data).dbs.db;
-} catch (error) {
-  console.error("Error fetching performance details:", error);
-  throw new Error("데이터를 불러오는 중 오류가 발생했습니다.");
-}
+  try {
+    const { data } = await playApi.get("/", {
+      params: {
+        service: import.meta.env.VITE_KOPIS_KEY,
+        stdate: getDateString(),
+        eddate: getDateString(),
+        rows: 1000,
+        cpage: 1,
+      },
+    });
+    return parseXMLToJSON(data).dbs.db;
+  } catch (error) {
+    console.error("Error fetching performance details:", error);
+    throw new Error("데이터를 불러오는 중 오류가 발생했습니다.");
+  }
 };
 
 export const getGenreData = async (genre) => {
@@ -44,18 +35,17 @@ export const getGenreAreaData = async (genre, area, start, end) => {
   let url = `?_start=${start}&_end=${end}`;
   if (genre !== "장르별") url += `&genrenm=${genre}`;
   if (area !== "지역별") url += `&area=${area}`;
-  const { data } = await playApi.get("/",{
-    params : {
+  const { data } = await playApi.get("/", {
+    params: {
       service: import.meta.env.VITE_KOPIS_KEY,
       stdate: getDateString(),
       eddate: getDateString(),
-      rows: 1000,
+      rows: 50,
       cpage: 1,
-      shcate: genre === '장르별' ? null : genre,
-      signgucode: area === '지역별' ? null : area,
-    }
+      shcate: genre === "장르별" ? null : genre,
+      signgucode: area === "지역별" ? null : area,
+    },
   });
-  console.log(parseXMLToJSON(data).dbs.db)
   return parseXMLToJSON(data).dbs.db;
 };
 
