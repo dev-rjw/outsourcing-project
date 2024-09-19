52a65691207ccb678730ba95050efd0e48e8f405
dev-rjw <rjw0105@gmail.com>
2024-09-19 09:03:18 +0900

    [fix] 카테고리 수정 시 리렌더링 되도록 수정, 더보기 클릭 시 스크롤 위로가지 않도록 수정, 검색 버튼 클릭 시 검색되도록 수정

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,73 +1,78 @@
 import { useQuery } from "@tanstack/react-query";
 import { searchGenreAreaData } from "../../api/playApi";
 import { areaCodes, genreCodes } from "../../utils/Kopis-api-common";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Category = () => {
-    const [searchValue, setSearchValue] = useState("");
-    const [genre, setGenre] = useState("장르별");
-    const [area, setArea] = useState("지역별");
-    const [end, setEnd] = useState(50);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchTerm, setSearchTerm] = useState(""); // 검색 버튼을 눌렀을 때의 값을 저장할 상태
+  const [genre, setGenre] = useState("장르별");
+  const [area, setArea] = useState("지역별");
+  const [end, setEnd] = useState(50);
 
-    /* TODO:
-    - 카테고리 변경 시 바로 값 바뀌도록 수정
-    - css 수정
-    */
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ["category"],
-        queryFn: () => searchGenreAreaData(searchValue, genre, area, 0, end),
-    });
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["category"],
+    queryFn: () => searchGenreAreaData(searchTerm, genre, area, 0, end),
+    keepPreviousData: true,
+  });
 
-    const addList = () => {
-        setEnd(end + 50);
-        searchGenreAreaData(searchValue, genre, area, 0, end);
-    };
+  const handleSearch = () => {
+    setSearchTerm(searchValue);
+  };
 
-    if (isLoading) {
-        return <div>로딩중입니다...</div>;
-    }
+  const addList = () => {
+    setEnd(end + 50);
+  };
 
-    if (isError) {
-        return <div>에러입니다...</div>;
-    }
+  useEffect(() => {
+    refetch();
+  }, [searchTerm, genre, area, end, refetch]);
 
-    return (
-        <div>
-            <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
-            <button onClick={() => searchGenreAreaData(searchValue, genre, area, 0, end)}>검색</button>
-            <div>
-                <select value={area} onChange={(e) => setArea(e.target.value)} selected="지역별" name="지역별">
-                    <option value="지역별">지역별</option>
-                    {Object.values(areaCodes).map((element, index) => {
-                        return (
-                            <option key={index} value={element}>
-                                {element}
-                            </option>
-                        );
-                    })}
-                </select>
-                <select value={genre} onChange={(e) => setGenre(e.target.value)} selected="장르별" name="장르별">
-                    <option value="장르별">장르별</option>
-                    {Object.values(genreCodes).map((element, index) => {
-                        return (
-                            <option key={index} value={element}>
-                                {element}
-                            </option>
-                        );
-                    })}
-                </select>
-                {data?.map((element) => {
-                    return (
-                        <div key={element.id}>
-                            <img src={element.poster} width="10%" />
-                            <div>{element.prfnm}</div>
-                        </div>
-                    );
-                })}
-                {data.length === 50 && <button onClick={addList}>더보기</button>}
+  if (isLoading) {
+    return <div>로딩중입니다...</div>;
+  }
+
+  if (isError) {
+    return <div>에러입니다...</div>;
+  }
+
+  return (
+    <div>
+      <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+      <button onClick={handleSearch}>검색</button>
+      <div>
+        <select value={area} onChange={(e) => setArea(e.target.value)} selected="지역별" name="지역별">
+          <option value="지역별">지역별</option>
+          {Object.values(areaCodes).map((element, index) => {
+            return (
+              <option key={index} value={element}>
+                {element}
+              </option>
+            );
+          })}
+        </select>
+        <select value={genre} onChange={(e) => setGenre(e.target.value)} selected="장르별" name="장르별">
+          <option value="장르별">장르별</option>
+          {Object.values(genreCodes).map((element, index) => {
+            return (
+              <option key={index} value={element}>
+                {element}
+              </option>
+            );
+          })}
+        </select>
+        {data?.map((element) => {
+          return (
+            <div key={element.id}>
+              <img src={element.poster} width="10%" />
+              <div>{element.prfnm}</div>
             </div>
-        </div>
-    );
+          );
+        })}
+        {data.length === end && <button onClick={addList}>더보기</button>}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
