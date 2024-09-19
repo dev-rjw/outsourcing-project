9c42ea278b7f183eb8f2b8f408cd9d73191e858c
joycie416 <joycie416@gmail.com>
2024-09-19 14:41:41 +0900

    [update] 캐러셀 카드 클릭시 상세페이지 이동

--- src/pages/main-page/Embla.jsx
+++ src/pages/main-page/Embla.jsx
@@ -4,6 +4,7 @@ import Autoplay from 'embla-carousel-autoplay';
 import styled from 'styled-components';
 import { getData } from '../../api/playApi';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 
 // const BASE_URL = "http://kopis.or.kr/openApi/restful/pblprfr";
 // const API_KEY = import.meta.env.VITE_KOPIS_KEY;
@@ -69,15 +70,20 @@ export default Embla
 
 //===============================================
 const Slide = ({ play }) => {
-  const { prfnm: title1, poster: poster1, fcltynm:place1, prfpdfrom: from1, prfpdto:to1 } = play[0];
-  const { prfnm: title2, poster: poster2, fcltynm:place2, prfpdfrom: from2, prfpdto:to2 } = play[1];
+  const { mt20id:id1, prfnm: title1, poster: poster1, fcltynm:place1, prfpdfrom: from1, prfpdto:to1 } = play[0];
+  const { mt20id:id2, prfnm: title2, poster: poster2, fcltynm:place2, prfpdfrom: from2, prfpdto:to2 } = play[1];
+
+  const navigate = useNavigate();
+  const handleClick = (id) => {
+    navigate(`/detail/${id}`)
+  }
   return (
     // <div className='recent-play min-w-0'> {/*h-[300px] grow-0 shrink-0 basis-full*/}
     <div className='embla__slide'> {/*grid*/}
       {/* <div className='cards w-full h-[400px] grid grid-cols-[1fr 1fr] bg-gray-300'> */}
       <div className='cards w-full h-[400px] flex gap-x-10'>
         {/* <img src={poster} /> */}
-        <div className='card'>
+        <div className='card cursor-pointer' onClick={() =>{handleClick(id1)}}>
           <img src={poster1} />
           <div className='card-content'>
             <h3>{title1}</h3>
@@ -85,7 +91,7 @@ const Slide = ({ play }) => {
             <p>{from1 === to1 ? from1 : `${from1} - ${to1}`}</p>
           </div>
         </div>
-        <div className='card'>
+        <div className='card cursor-pointer' onClick={() =>{handleClick(id2)}}>
           <img src={poster2} />
           <div className='card-content'>
             <h3>{title2}</h3>
