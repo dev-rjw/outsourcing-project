5d3ebba8d98bc4ca7db8673ef87bccc34b6e4059
ekyeon715 <rkdwldus9636@naver.com>
2024-09-19 11:08:45 +0900

    [update] 헤더에 프로필,로그아웃, 로그인 버튼 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useUserStore from "../zustand/useUserStore";
 
 const Header = () => {
+  const { user, clearUser } = useUserStore();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    clearUser();
+    navigate("/login");
+  };
   return (
     <>
       <div>
@@ -9,7 +18,14 @@ const Header = () => {
       <div>
         <Link to="/community">커뮤니티</Link>
         <Link to="/category">카테고리</Link>
-        <button>로그아웃</button>
+        {user ? (
+          <>
+            <Link to="/profile">프로필</Link>
+            <button onClick={handleLogout}>로그아웃</button>
+          </>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
       </div>
     </>
   );
