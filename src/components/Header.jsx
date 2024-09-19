903e3a628010ff29eae4be5d8491c73b70b4b3e7
joycie416 <joycie416@gmail.com>
2024-09-19 10:20:52 +0900

    [fix] Layout.jsx 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,20 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <div>
-        <Link to="/">홈</Link>
-      </div>
-      <div>
-        <Link to="/community">커뮤니티</Link>
-        <Link to="/category">카테고리</Link>
-        <button>로그아웃</button>
-      </div>
-    </>
+    <header className="flex-col m-4">
+      <Link to="/">커튼콜</Link>
+      <nav className="flex ">
+        <div className="flex ">
+          <Link to="/category">장르별</Link>
+          <Link to="/category">장소별</Link>
+          <Link to="/community">커뮤니티</Link>
+        </div>
+        <div className="flex">
+          <input placeholder="검색내용입력"></input>
+          <button>검색이모지</button>
+        </div>
+      </nav>
+    </header>
   );
 };
 
