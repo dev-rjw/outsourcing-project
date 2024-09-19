a60706da9ff5e4d99d948daf409cb9ada11b3d92
ekyeon715 <rkdwldus9636@naver.com>
2024-09-19 09:29:24 +0900

    [update] 프로필 페이지 라우터 설정 추가

--- src/shared/Route.jsx
+++ src/shared/Route.jsx
@@ -7,6 +7,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Login from "../pages/login/Login";
 import SignUp from "../pages/signup/Signup";
+import Profile from "../pages/profile/Profile";
 
 const RouterComponent = () => {
   return (
@@ -19,6 +20,7 @@ const RouterComponent = () => {
         <Route path="/community" element={<Community />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </BrowserRouter>
