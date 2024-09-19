27500de99646d3bd6900da52737632e49b62caf1
dev-rjw <rjw0105@gmail.com>
2024-09-19 19:54:57 +0900

    [feat] 하드코딩 상수화 및 css 수정

--- src/api/playApi.js
+++ src/api/playApi.js
@@ -4,9 +4,10 @@ import { getDateString, parseXMLToJSON } from "../utils/utils";
 
 // KOPIS 관련
 const BASE_URL = "http://kopis.or.kr/openApi/restful/pblprfr";
-// const BASE_DB_URL = import.meta.env.VITE_DB_URL + "/performances";
+const BASE_DB_URL = import.meta.env.VITE_DB_URL + "/performances";
 
 const playApi = axios.create({ baseURL: BASE_URL });
+const playJsApi = axios.create({ baseURL: BASE_DB_URL });
 
 export const getData = async () => {
   try {
@@ -15,7 +16,7 @@ export const getData = async () => {
         service: import.meta.env.VITE_KOPIS_KEY,
         stdate: getDateString(),
         eddate: getDateString(),
-        rows: 1000,
+        rows: 100,
         cpage: 1,
       },
     });
@@ -35,18 +36,8 @@ export const getGenreAreaData = async (genre, area, start, end) => {
   let url = `?_start=${start}&_end=${end}`;
   if (genre !== "장르별") url += `&genrenm=${genre}`;
   if (area !== "지역별") url += `&area=${area}`;
-  const { data } = await playApi.get("/", {
-    params: {
-      service: import.meta.env.VITE_KOPIS_KEY,
-      stdate: getDateString(),
-      eddate: getDateString(),
-      rows: 50,
-      cpage: 1,
-      shcate: genre === "장르별" ? null : genre,
-      signgucode: area === "지역별" ? null : area,
-    },
-  });
-  return parseXMLToJSON(data).dbs.db;
+  const { data } = await playJsApi.get(url);
+  return data;
 };
 
 export const searchGenreAreaData = async (searchValue, genre, area, start, end) => {
--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -3,22 +3,33 @@ import { searchGenreAreaData } from "../../api/playApi";
 import { areaCodes, genreCodes } from "../../utils/Kopis-api-common";
 import { useEffect, useState } from "react";
 import CategorySelect from "./CategorySelect";
+import QUERY_KEY from "../../constants/queryKey";
+import { useNavigate } from "react-router-dom";
 
 const Category = () => {
+  const navigate = useNavigate();
+
+  const GENRE = "장르별";
+  const AREA = "지역별";
+
   const [searchValue, setSearchValue] = useState("");
   const [searchTerm, setSearchTerm] = useState(""); // 검색 버튼을 눌렀을 때의 값을 저장할 상태
-  const [genre, setGenre] = useState("장르별");
-  const [area, setArea] = useState("지역별");
+  const [genre, setGenre] = useState(GENRE);
+  const [area, setArea] = useState(AREA);
   const [end, setEnd] = useState(50);
 
   const { data, isLoading, isError, refetch } = useQuery({
-    queryKey: ["category"],
+    queryKey: [QUERY_KEY.category],
     queryFn: () => searchGenreAreaData(searchTerm, genre, area, 0, end),
     keepPreviousData: true,
   });
 
   const handleSearch = () => {
     setSearchTerm(searchValue);
+  };
+
+  const handleCardClick = (play) => {
+    navigate(`/detail/${play.mt20id}`);
   };
 
   const addList = () => {
@@ -38,18 +49,18 @@ const Category = () => {
   }
 
   return (
-    <div>
+    <div className="max-w-screen-lg">
       <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
       <button onClick={handleSearch}>검색</button>
       <div>
-        <CategorySelect state={area} setState={setArea} categoryName="지역별" codes={areaCodes} />
-        <CategorySelect state={genre} setState={setGenre} categoryName="장르별" codes={genreCodes} />
+        <CategorySelect state={area} setState={setArea} categoryName={AREA} codes={areaCodes} />
+        <CategorySelect state={genre} setState={setGenre} categoryName={GENRE} codes={genreCodes} />
       </div>
       <div className="grid grid-cols-5 w-[1000px]">
         {data.length !== 0 ? (
           data.map((element) => {
             return (
-              <div className="flex flex-col items-center justify-start h-[300px] " key={element.id}>
+              <div className="flex flex-col items-center justify-start h-[300px] cursor-pointer" key={element.mt20id} onClick={() => handleCardClick(element)}>
                 <img src={element.poster} className="w-4/5 h-4/5" />
                 <div>{element.prfnm}</div>
               </div>
