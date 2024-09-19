2568561db2a5521ce867e405275a3f35489ba14f
dev-rjw <rjw0105@gmail.com>
2024-09-19 19:46:42 +0900

    [fix] query key 수정

--- src/constants/queryKey.js
+++ src/constants/queryKey.js
@@ -1,7 +1,7 @@
 const QUERY_KEY = {
-  posts: ["posts"],
-  category: ["category"],
-  comments: ["comments"],
+  posts: "posts",
+  category: "category",
+  comments: "comments",
 };
 
 export default QUERY_KEY;
