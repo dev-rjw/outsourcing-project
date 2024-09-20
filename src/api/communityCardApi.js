ed3b5250e15570afdcca937ffbb3755078741f9e
ekyeon715 <rkdwldus9636@naver.com>
2024-09-20 20:54:45 +0900

    [fix] 서버 주소 변경

--- src/api/communityCardApi.js
+++ src/api/communityCardApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/communityPosts";
+const API_URL = import.meta.env.VITE_DB_URL + "/communityPosts";
 
 export const fetchPosts = async () => {
   try {
