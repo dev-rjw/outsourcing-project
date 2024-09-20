6a43c2b751664c753e099232612edfa7f30570e2
ekyeon715 <rkdwldus9636@naver.com>
2024-09-20 16:52:06 +0900

    [update] 업데이트 로직 추가

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,20 +1,27 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getUserProfile, updateProfile } from "../../api/auth";
 import useUserStore from "../../zustand/useUserStore";
 
 const Profile = () => {
   const queryClient = useQueryClient();
-  const { user, setUser } = useUserStore();
-  const [nickname, setNickname] = useState();
+  const { accessToken } = useUserStore();
+  const [nickname, setNickname] = useState("");
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ["profiles"],
-    queryFn: getUserProfile,
+    queryFn: () => getUserProfile(accessToken),
   });
 
-  const mutation = useMutation({
-    mutationFn: updateProfile,
+  // 초기 프로필 값 설정
+  useEffect(() => {
+    if (data) {
+      setNickname(data.nickname);
+    }
+  }, [data]);
+
+  const updateMutation = useMutation({
+    mutationFn: (formData) => updateProfile(formData, accessToken),
     onSuccess: () => {
       queryClient.invalidateQueries(["profiles"]);
     },
@@ -36,8 +43,9 @@ const Profile = () => {
     e.preventDefault();
     try {
       const formData = { nickname };
-      const updataUser = await updateProfile(formData, user.accessToken);
-      setUser(updataUser);
+      await updateMutation.mutateAsync(formData);
+      // const updataUser = await updateProfile(formData, user.accessToken);
+      // setUser(updataUser);
       alert("프로필이 성공적으로 업데이트 되었습니다.");
     } catch (error) {
       alert("프로필 업데이트에 실패했습니다. 다시 시도해주세요.");
@@ -50,7 +58,7 @@ const Profile = () => {
         <form onSubmit={handleSubmit}>
           <div>
             <label>닉네임</label>
-            <input onChange={handleNicknameChange} />
+            <input value={nickname} onChange={handleNicknameChange} />
           </div>
           <button type="submit">프로필 업데이트</button>
         </form>
